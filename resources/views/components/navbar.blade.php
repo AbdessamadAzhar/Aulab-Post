e7bebfbbd5295e18d1038dd3a9bd53ec5aad26b2
Massimo Biffi <mbiffi3@gmail.com>
2023-03-21 19:10:44 +0100

    navbar modificata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,41 +1,49 @@
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{('homepage')}}">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        @Auth
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Benvenuto {{Auth::user()->name}}
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/">profilo</a></li>            
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        @else
-      </ul>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Benvenuto
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
-          <li><a class="dropdown-item"  href="{{route('login')}}">Accedi</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-        @endauth
+<nav class="navbar navbar-expand-lg bg-info text-white ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ 'homepage' }}">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                @Auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Benvenuto {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/">profilo</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.queryselector('#form-logout').submit()";>Logout</a></li>
+                            <form method = "POST" action="{{ route('logout') }}" id="form-logout" class="d-none">
+                                @csrf
+                            </form>
+                        </ul>
+                    </li>
+                @endauth
+                @guest
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Benvenuto
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
+                            <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
