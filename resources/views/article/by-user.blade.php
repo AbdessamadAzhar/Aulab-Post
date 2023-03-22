3c59be0913482fd6a898516cd167fc5bf9411a6e
Massimo Biffi <mbiffi3@gmail.com>
2023-03-22 18:09:38 +0100

    user fatto

--- resources/views/article/by-user.blade.php
+++ resources/views/article/by-user.blade.php
@@ -1,34 +1,31 @@
 <x-layout>
-    @if (session('message'))
-        <div class="alert alert-success text-center">
-            {{ session('message') }}
+        <div class="container-fluid p-5 bg-info text-center text-white">
+        <div class="row justify-content-center">
+            <h1 class="display-1 text-capitalize">
+                User {{$user->name}}
+            </h1>
         </div>
-    @endif
-    <x-header>
-        Aulab Post!
-    </x-header>
+    </div>
 
     <div class="container my-5">
         <div class="row justify-content-around">
             @foreach ($articles as $article)
-                <div class="col-12 col-md-3">
+                <div class="col-12 col-md-3 my-2">
                     <div class="card">
                         <img src="{{ Storage::url($article->image) }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $article->title }}</h5>
                             <p class="card-text">{{ $article->subtitle }}</p>
-                            <a href="{{route('article.byCategory', ['category'=>$article->category->id])}}" class="small text-muted fst-italic text capitalize">{{$article->category->name}}</a>
-                            <a href="{{route('article.byUser', ['user'=>$article->user->id])}}" class="small text-muted fst-italic text capitalize">{{$article->user->name}}</a>
                         </div>
                         <div class="card-footer text-muted d-flex justify-content-beetween align-items-center">
                             Redatto il {{$article->created_at->format('d/m/y')}} da {{$article->user->name}}
-                            <a href="{{route('article.show', compact('article'))}}" class="btn btn-info text-white">Leggi</a> 
+                            <a href="{{route('article.show', compact('article'))}}" class="btn btn-info text-white">Leggi</a>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
-    </div> 
+    </div>
 
 
 </x-layout>
