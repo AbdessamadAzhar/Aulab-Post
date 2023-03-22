29a3fb6c48205c81b322580f5a116bd8d7b3804c
Massimo Biffi <mbiffi3@gmail.com>
2023-03-22 12:17:24 +0100

    inizio articolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,34 @@
 <x-layout>
-@if(session('message'))
-<div class="alert alert-success text-center">
-    {{session('message')}}
-</div>
-@endif
+    @if (session('message'))
+        <div class="alert alert-success text-center">
+            {{ session('message') }}
+        </div>
+    @endif
     <x-header>
         Aulab Post!
     </x-header>
-    
+
+    <div class="container my-5">
+        <div class="row justify-content-around">
+            @foreach ($articles as $article)
+                <div class="col-12 col-md-3">
+                    <div class="card">
+                        <img src="{{ Storage::url($article->image) }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $article->title }}</h5>
+                            <p class="card-text">{{ $article->subtitle }}</p>
+                            <p class="small text-muted fst-italic text-capitalized">{{ $article->category->name }}</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        <div class="card-footer text-muted d-flex justify-content-beetween align-items-center">
+                            Redatto il {{$article->created_at->format('d/m/y')}} da {{$article->user->name}}
+                            <a href="#" class="btn btn-info text-white">Leggi</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+
 </x-layout>
