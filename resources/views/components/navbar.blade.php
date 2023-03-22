53e58dac0925f1c8ce037937db472593d2e7ba93
Gianni Musardo <gianni939@hotmail.it>
2023-03-22 02:19:12 +0000

    piccolezze

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-info text-white ">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ 'homepage' }}">Navbar</a>
+        <a class="navbar-brand" href="{{ route('homepage') }}">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
