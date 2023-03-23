6596d868dc146efe7117c76135a714e1f70a7738
Massimo Biffi <mbiffi3@gmail.com>
2023-03-23 17:58:54 +0100

    message

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,33 +4,57 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class PublicController extends Controller
 {
-    public function homepage(){
-        $articles = Article::orderBy('created_at','desc')->take(4)->get();
-    return view('welcome', compact('articles'));
+    public function homepage()
+    {
+        $articles = Article::orderBy('created_at', 'desc')->take(4)->get();
+        return view('welcome', compact('articles'));
     }
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth')->except('homepage');
-
     }
-        public function careers(){
-            return view('careers');
-        }
-
-        public function careersSubmit(Request $request){
-            $request->validate([
-
-                'role' => 'required',
-                'email' => 'required',
-                'message' => 'required',
-            ]);
-        }
-
+    public function careers()
+    {
+        return view('careers');
     }
 
 
+    public function careersSubmit(Request $request)
+    {
+        $request->validate([
 
+            'role' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+        ]);
 
+        $user = Auth::user();
+        $role = $request->role;
+        $email = $request->email;
+        $message = $request->message;
+
+        Mail::to('admin@theaulabpost.it')->send(new CarrerRequestMail(compact('role', 'email', 'message')));
+
+        switch ($role) {
+            case 'admin':
+                $user->is_admin = Null;
+
+            case 'revisor':
+                $user->is_revisor = Null;
+
+            case 'writer':
+                $user->is_writer = Null;
+        }
+
+        $user->update();
+
+        return redirect(route('homepage'))->with('message', 'Grazie per averci contatatto');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/article/index', [ArticleController::class, 'index'])->name('article
 Route::get('article/show/{article}', [ArticleController::class, 'show'])->name('article.show');
 Route::get('/article/category/{category}',[ArticleController::class,'byCategory'])->name('article.byCategory');
 Route::get('/article/user/{user}',[ArticleController::class,'byUser'])->name('article.byUser');
+
+Route::middleware('admin')->group(function(){
+    Route::get('\admin\dashboard')
+})
