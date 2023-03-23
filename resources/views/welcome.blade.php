7c78a2f01efe2f49af5864aca81ed623153a0cd5
AbdessamadAzhar <abdessamadazhar00@gmail.com>
2023-03-23 15:00:57 +0100

    modifica file welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
             @foreach ($articles as $article)
                 <div class="col-12 col-md-3">
                     <div class="card">
-                        <img src="{{ Storage::url($article->image) }}" class="card-img-top" alt="...">
+                        <img src="{{Storage::url($article->image) }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $article->title }}</h5>
                             <p class="card-text">{{ $article->subtitle }}</p>
