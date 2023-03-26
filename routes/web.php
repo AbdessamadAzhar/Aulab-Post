ebe4f0989fc33f80bdf0eef66176daf56878c2cc
Massimo Biffi <mbiffi3@gmail.com>
2023-03-26 12:57:25 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,3 @@ Route::middleware('revisor')->group(function(){
         
 });
 
-Route::middleware('writer')->group(function(){
-    Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
-    Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
-
-});
