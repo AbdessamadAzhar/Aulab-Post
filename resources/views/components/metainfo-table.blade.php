d33cabb4df3e7c515f2fb58f8a579489437208f2
Massimo Biffi <mbiffi3@gmail.com>
2023-03-29 12:19:59 +0200

    ;

--- resources/views/components/metainfo-table.blade.php
+++ resources/views/components/metainfo-table.blade.php
@@ -14,46 +14,47 @@
                 <th scope="row">{{ $metaInfo->id }}</th>
                 <td>{{ $metaInfo->name }}</td>
                 <td>{{ count($metaInfo->articles) }}</td>
-                @if($metaType == "tags")
-                <td>
-                   <form action="{{route('admin.editTag',['tag'=> $metaInfo])}}" method="POST">
-                    @csrf
-                    @method('put')
-                    <input type="text" name="name" placeholder="Nuovo nome tag" class="form-control w-50 d-inline">
-                    <button type="submit" class="btn btn-info text-white">Aggiorna</button>
-                    </form>
-                </td>
-                <td>
-                <form action="{{route('admin.deleteTag', ['tag' => $metaInfo])}}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger text-white">Elimina</button>
-                </form>
-            </td>
-            @else
-            <td>
-            <form action="{{route('admin.editCategory', ['category' => $metaInfo])}}" method="POST">
-                    @csrf
-                    @method('put')
-                    <input type="text" name="name" placeholder="Nuovo nome categoria" class="form-control w-50 d-inline">
-                    <button type="submit" class="btn btn-danger text-white">Aggiorna</button>
-                </form>
-            </td>
-            <td>
-                <form action="{{route('admin.deleteCategory',['category'=> $metaInfo])}}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger text-white">Elimina</button>
-                </form>
-            </td>
-                    @endif
-                </tr>
-            @endforeach
-            <form class="d-flex" action="{{route('admin.storeCategory')}}" method="POST">
-                @csrf
-                <input type="text" name="name" class="form-control me-2" placeholder="Inserisci una nuova categoria">
-                <button type="submit" class="btn btn-success text-white">Aggiungi</button>
-            </form>
+                @if ($metaType == 'tags')
+                    <td>
+                        <form action="{{ route('admin.editTag', ['tag' => $metaInfo]) }}" method="POST">
+                            @csrf
+                            @method('put')
+                            <input type="text" name="name" placeholder="Nuovo nome tag"
+                                class="form-control w-50 d-inline">
+                            <button type="submit" class="btn btn-info text-white">Aggiorna</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="{{ route('admin.deleteTag', ['tag' => $metaInfo]) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger text-white">Elimina</button>
+                        </form>
+                    </td>
+                @else
+                    <td>
+                        <form action="{{ route('admin.editCategory', ['category' => $metaInfo]) }}" method="POST">
+                            @csrf
+                            @method('put')
+                            <input type="text" name="name" placeholder="Nuovo nome categoria"
+                                class="form-control w-50 d-inline">
+                            <button type="submit" class="btn btn-danger text-white">Aggiorna</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="{{ route('admin.deleteCategory', ['category' => $metaInfo]) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger text-white">Elimina</button>
+                        </form>
+                    </td>
+                @endif
+            </tr>
+        @endforeach
+        <form class="d-flex" action="{{ route('admin.storeCategory') }}" method="POST">
+            @csrf
+            <input type="text" name="name" class="form-control me-2" placeholder="Inserisci una nuova categoria">
+            <button type="submit" class="btn btn-success text-white">Aggiungi</button>
+        </form>
     </tbody>
 </table>
-
